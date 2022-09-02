ea12e326744edc68b19021a9ca53f9411b35634a
Attinder singh <attinderkahlon@gmail.com>
2022-09-03 09:52:26 +1000

    tailwind installation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import "./App.css"
+import Nav from "./components/Nav"
 
 function App() {
-  return <div className='App'></div>
+  return (
+    <div>
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <h2 className='underline'>new heading</h2>
+      <Nav />
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Nav = () => {
+  return (
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Links</li>
+    </nav>
+  )
+}
+
+export default Nav
