6db1b98c99d1aa87c5f5c4bad7d818b06020b4f2
Attinder singh <attinderkahlon@gmail.com>
2022-09-08 04:54:49 +1000

    nav bar fix

--- src/components/assets/Dropdown.js
+++ src/components/assets/Dropdown.js
@@ -1,70 +1,83 @@
-import React from "react"
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
-const Dropdown = () => {
-  const [open, setOpen] = useState(false)
+function Dropdown() {
+  const ref = useRef()
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  useEffect(() => {
+    const checkIfClickedOutside = (e) => {
+      if (isMenuOpen && ref.current && !ref.current.contains(e.target)) {
+        setIsMenuOpen(false)
+      }
+    }
+
+    document.addEventListener("mousedown", checkIfClickedOutside)
+
+    return () => {
+      // Cleanup the event listener
+      document.removeEventListener("mousedown", checkIfClickedOutside)
+    }
+  }, [isMenuOpen])
 
   return (
-    <div>
-      <div className='relative inline-block text-left'>
-        <div>
-          <button
-            type='button'
-            className='inline-flex w-full justify-center rounded-md border border-gray-300  px-4 py-2 text-sm font-medium shadow-sm hover: focus:outline-none '
-            id='menu-button'
-            aria-expanded='true'
-            aria-haspopup='true'
-            onClick={() => setOpen(!open)}
-          >
-            Links
-            <svg
-              className='-mr-1 ml-2 h-5 w-5'
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 20 20'
-              fill='currentColor'
-              aria-hidden='true'
-            >
-              <path
-                fillRule='evenodd'
-                d='M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z'
-                clipRule='evenodd'
-              />
-            </svg>
-          </button>
-        </div>
-        {open ? (
-          <div
-            className='absolute left-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-gray-700 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'
-            role='menu'
-            aria-orientation='vertical'
-            aria-labelledby='menu-button'
-            tabIndex='-1'
-          >
-            <div className='py-1' role='none'>
-              <a
-                href='https://github.com/Attinderkahlon'
-                className=' block px-4 py-2 text-sm'
-                role='menuitem'
-                tabIndex='-1'
-                id='menu-item-0'
-              >
-                Github
-              </a>
-              <a
-                href='https://www.linkedin.com/in/attinder18/'
-                className=' block px-4 py-2 text-sm'
-                role='menuitem'
-                tabIndex='-1'
-                id='menu-item-1'
-              >
-                Linkedin
-              </a>
+    <div className='relative inline-block text-left' ref={ref}>
+      <button
+        type='button'
+        className='inline-flex w-full rounded-md  px-2 py-2 text-l font-medium shadow-sm hover: focus:outline-none hover:border'
+        id='menu-button'
+        aria-expanded='true'
+        aria-haspopup='true'
+        onClick={() => setIsMenuOpen((oldState) => !oldState)}
+      >
+        Links
+        <svg
+          className='-mr-1 ml-2 h-5 w-5'
+          xmlns='http://www.w3.org/2000/svg'
+          viewBox='0 0 20 20'
+          fill='currentColor'
+          aria-hidden='true'
+        >
+          <path
+            fillRule='evenodd'
+            d='M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z'
+            clipRule='evenodd'
+          />
+        </svg>
+      </button>
 
-              <form method='POST' action='#' role='none'></form>
-            </div>
+      {isMenuOpen && (
+        <div
+          className='absolute left-0 z-10 mt-2 w-24 origin-top-right rounded-md bg-gray-700 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'
+          role='menu'
+          aria-orientation='vertical'
+          aria-labelledby='menu-button'
+          tabIndex='-1'
+        >
+          <div className='py-1' role='none'>
+            <a
+              href='https://github.com/Attinderkahlon'
+              className=' block px-4 py-2 text-sm'
+              role='menuitem'
+              tabIndex='-1'
+              id='menu-item-0'
+            >
+              Github
+            </a>
+            <a
+              href='https://www.linkedin.com/in/attinder18/'
+              className=' block px-4 py-2 text-sm'
+              role='menuitem'
+              tabIndex='-1'
+              id='menu-item-1'
+            >
+              Linkedin
+            </a>
+
+            <form method='POST' action='#' role='none'></form>
           </div>
-        ) : null}
-      </div>
+        </div>
+      )}
     </div>
   )
 }
--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -4,14 +4,21 @@ import Dropdown from "../assets/Dropdown"
 const Nav = () => {
   return (
     <nav className='text-xl  text-slate-50 bg-gray-700 font-Montserrat flex py-4'>
-      <img
+      {/* <img
         src='images/pp.png'
         alt='profile'
         className='object-contain h-12 w-12 rounded-full mx-12'
-      />
-      <ul className='flex gap-6 py-2'>
-        <li>Home</li>
-        <li>About</li>
+      /> */}
+      <ul className='flex-col flex sm:flex-row gap-6 px-2 py-2  mx-12'>
+        <li>
+          <img
+            src='images/pp.png'
+            alt='profile'
+            className='object-contain h-12 w-12 rounded-full'
+          />
+        </li>
+        {/* <li>Home</li> */}
+        <li className='px-2 py-2'>About</li>
         <li>
           <Dropdown />
         </li>
